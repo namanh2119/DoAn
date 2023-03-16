eb139304bdff568d60d786433e4a247fa549eee8
Hg Nam <namlai2119@gmail.com>
2023-03-17 00:17:25 +0700

    modified:   src/App.js 	modified:   src/components/Footer/index.js 	modified:   src/components/Header/index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,12 +4,20 @@ import styles from './Header.module.scss';
 
 const cx = classNames.bind(styles);
 function Header() {
+    const lists = ['Trang chủ', 'Món ăn', 'Đồ uống', 'Liên hệ'];
+
     return (
         <header className={cx('header')}>
             <div className={cx('header__content')}>
                 <div className={cx('header__left')}>
                     <div className={cx('logo')}></div>
-                    <ul className={cx('header__list')}></ul>
+                    <ul className={cx('header__list')}>
+                        {lists.map((list, index) => (
+                            <li key={index} className={cx('header__list-item')}>
+                                {list}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
                 <div className={cx('header__right')}></div>
             </div>
