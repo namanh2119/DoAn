2bc8f573ffb2f0c0895b399e65cee73debdc7b28
Hg Nam <namlai2119@gmail.com>
2023-05-06 17:02:57 +0700

    modified:   src/components/pages/Account/Account.module.scss 	modified:   src/components/pages/Account/index.js 	modified:   src/components/pages/Contact/Contact.module.scss 	modified:   src/components/pages/Contact/index.js

--- src/components/pages/Account/index.js
+++ src/components/pages/Account/index.js
@@ -1,10 +1,88 @@
+import { Link } from 'react-router-dom';
 import styles from './Account.module.scss';
 import classNames from 'classnames/bind';
+import { useRef } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Account() {
-    return <div>Tài Khoản</div>;
+    const inputRef = useRef(null);
+    const handleClick = () => {
+        inputRef.current.click();
+    };
+    const handleFileChange = (event) => {
+        const fileObj = event.target.files && event.target.files[0];
+        if (!fileObj) {
+            return;
+        }
+        console.log('fileObj is', fileObj);
+
+        // 👇️ reset file input
+        event.target.value = null;
+
+        // 👇️ is now empty
+        console.log(event.target.files);
+
+        // 👇️ can still access file object here
+        console.log(fileObj);
+        console.log(fileObj.name);
+    };
+
+    return (
+        <div className={cx('account', 'grid', 'wide')}>
+            <div className={cx('account__content', 'row')}>
+                <div className={cx('avatar', 'l-5', 'col')}>
+                    <input
+                        className={cx('avatar-user')}
+                        style={{ backgroundImage: `url('')` }}
+                        ref={inputRef}
+                        onChange={handleFileChange}
+                    />
+                    <button className={cx('btn')} onClick={handleClick}>
+                        Thay đổi Avatar
+                    </button>
+                </div>
+                <div className={cx('infomation', 'l-7', 'col')}>
+                    <div className={cx('form-user')}>
+                        <p className={cx('title-user')}>Họ và tên:</p>
+                        <input type="text" />
+                    </div>
+                    <div className={cx('form-user')}>
+                        <p className={cx('title-user')}>Số điện thoại:</p>
+                        <input type="text" />
+                    </div>
+                    <div className={cx('form-user')}>
+                        <p className={cx('title-user')}>Mail:</p>
+                        <input type="text" />
+                    </div>
+                    <div className={cx('address', 'row', 'col')}>
+                        <div className={cx('form-user', 'l-6', 'col')}>
+                            <p className={cx('title-user')}>Tỉnh/Thành Phố:</p>
+                            <input type="text" />
+                        </div>
+                        <div className={cx('form-user', 'l-6', 'col')}>
+                            <p className={cx('title-user')}>Quận/Huyện:</p>
+                            <input type="text" />
+                        </div>
+                        <div className={cx('form-user', 'l-6', 'col')}>
+                            <p className={cx('title-user')}>Xã/Phường:</p>
+                            <input type="text" />
+                        </div>
+                        <div className={cx('form-user', 'l-6', 'col')}>
+                            <p className={cx('title-user')}>Số nhà/Ngõ:</p>
+                            <input type="text" />
+                        </div>
+                    </div>
+                    <div className={cx('button')}>
+                        <button className={cx('btn')}>Cập nhật</button>
+                        <Link to="/" className={cx('btn')}>
+                            Đăng xuất
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Account;
--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -36,6 +36,7 @@ function Contact() {
                 <div className={cx('col l-6 t-1')}>
                     <div className={cx('map')}>
                         <iframe
+                            title="map"
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.5043321826743!2d105.88728148906797!3d21.052509982579306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135a9061ed78511%3A0x7798ca50c379c9ef!2sCT1A%20Homeland%20Long%20Bi%C3%AAn!5e0!3m2!1svi!2s!4v1675000874253!5m2!1svi!2s"
                             allowFullScreen=""
                             loading="lazy"
